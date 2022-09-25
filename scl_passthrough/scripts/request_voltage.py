7cc6b18a97e843658f35268e8ebdd59bc1613a80
e71828 <740943170@qq.com>
2022-09-25 21:01:31 +0800

    debug scl

--- scl_passthrough/scripts/request_voltage.py
+++ scl_passthrough/scripts/request_voltage.py
@@ -13,10 +13,15 @@ if __name__ == '__main__':
         request_voltage = rospy.ServiceProxy('scl_passthrough', scl)
         voltage_QA = request_voltage('IU')
         vol_str = voltage_QA.Answer
-        rospy.loginfo('voltage is %.1f V' % (int(vol_str[3::], 16) / 10))
+        if len(vol_str) > 2 and 'IU=' in vol_str:
+            index = vol_str.index('IU=')
+            rospy.loginfo('voltage is %.1f V' % (int(vol_str[index+3:index+7], 16) / 10))
+
+        # test MD
         voltage_QA = request_voltage('MD')
         vol_str = voltage_QA.Answer
-        rospy.loginfo("Receiving {} for MD.".format(vol_str))
+        if vol_str == '%\r':
+            rospy.loginfo("Motor disable OK with MD.")
 
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
