1c314d04064fcb6acf553d4b39a12fef82e45135
e71828 <740943170@qq.com>
2023-06-28 12:21:38 +0800

    adjust Hz

--- eimo_msgs/scripts/mouse_multi.py
+++ eimo_msgs/scripts/mouse_multi.py
@@ -22,59 +22,62 @@ def send_control_cmd():
                 )
             )
             print("".join(["buttons=", str(state.buttons)]))
-        pub = rospy.Publisher('control', control, queue_size=1)
-        rospy.init_node('publish_control_command', anonymous=True)
-        frequency = rospy.get_param('~frequency', default=1)
-        rate = rospy.Rate(frequency)  # 1hz
-        threshold = 0.9
-        gain = 64
-        while not rospy.is_shutdown():
-            forward, backward, right, left, up, down, button_0, button_1 = 0, 0, 0, 0, 0, 0, 0, 0
-            for _ in range(90):
-                state = pyspacemouse.read()
-                if state.y > threshold:
-                    forward = True
-                elif state.y < -threshold:
-                    backward = True
-                if state.yaw > threshold:
-                    right = True
-                elif state.yaw < -threshold:
-                    left = True
-                if state.z > threshold:
-                    up = True
-                elif state.z < -threshold:
-                    down = True
-                if state.buttons[0]:
-                    button_0 = True
-                if state.buttons[1]:
-                    button_1 = True
-                sleep(0.01)
 
-            if right and not left:
-                control_cmd = control(0, 0, gain, 0, 0, 0, 1, button_0, button_1)
-            elif left and not right:
-                control_cmd = control(0, 0, gain, 0, 0, 1, 0, button_0, button_1)
-            elif up and not down and not button_0 and not button_1:
-                control_cmd = control(0, 0, gain, 1, 0, 0, 0, 0, 0)
-            elif down and not up and not button_0 and not button_1:
-                control_cmd = control(0, 0, gain, 0, 1, 0, 0, 0, 0)
-            elif up and not down and button_0 and  button_1:
-                control_cmd = control(0, 0, gain, 1, 0, 0, 0, 1, 1)
-            elif down and not up and button_0 and  button_1:
-                control_cmd = control(0, 0, gain, 0, 1, 0, 0, 1, 1)
-            elif forward and not backward:
-                control_cmd = control(1, 0, gain, 0, 0, 0, 0, button_0, button_1)
-            elif backward and not forward:
-                control_cmd = control(0, 1, gain, 0, 0, 0, 0, button_0, button_1)
-            elif button_0 or button_1:
-                control_cmd = control(0, 0, gain, 0, 0, 0, 0, button_0, button_1)
-            else:
-                control_cmd = control(0, 0, gain, 0, 0, 0, 0, 0, 0)
-            rospy.loginfo(control_cmd)
-            pub.publish(control_cmd)
-            if not is_master_online():
-                rospy.signal_shutdown("ROS master is offline")
-            rate.sleep()
+            sleep(4) # wait here
+            pub = rospy.Publisher('control', control, queue_size=1)
+            rospy.init_node('publish_control_command', anonymous=True)
+            frequency = rospy.get_param('~frequency', default=1)
+            rate = rospy.Rate(frequency)  # 1hz
+            threshold = 0.9
+            gain = 64
+            rec_times = int(100/frequency)
+            while not rospy.is_shutdown():
+                forward, backward, right, left, up, down, button_0, button_1 = 0, 0, 0, 0, 0, 0, 0, 0
+                for _ in range(rec_times):
+                    state = pyspacemouse.read()
+                    if state.y > threshold:
+                        forward = True
+                    elif state.y < -threshold:
+                        backward = True
+                    if state.yaw > threshold:
+                        right = True
+                    elif state.yaw < -threshold:
+                        left = True
+                    if state.z > threshold:
+                        up = True
+                    elif state.z < -threshold:
+                        down = True
+                    if state.buttons[0]:
+                        button_0 = True
+                    if state.buttons[1]:
+                        button_1 = True
+                    sleep(0.3/rec_times)
+
+                if right and not left:
+                    control_cmd = control(0, 0, gain, 0, 0, 0, 1, button_0, button_1)
+                elif left and not right:
+                    control_cmd = control(0, 0, gain, 0, 0, 1, 0, button_0, button_1)
+                elif up and not down and not button_0 and not button_1:
+                    control_cmd = control(0, 0, gain, 1, 0, 0, 0, 0, 0)
+                elif down and not up and not button_0 and not button_1:
+                    control_cmd = control(0, 0, gain, 0, 1, 0, 0, 0, 0)
+                elif up and not down and button_0 and  button_1:
+                    control_cmd = control(0, 0, gain, 1, 0, 0, 0, 1, 1)
+                elif down and not up and button_0 and  button_1:
+                    control_cmd = control(0, 0, gain, 0, 1, 0, 0, 1, 1)
+                elif forward and not backward:
+                    control_cmd = control(1, 0, gain, 0, 0, 0, 0, button_0, button_1)
+                elif backward and not forward:
+                    control_cmd = control(0, 1, gain, 0, 0, 0, 0, button_0, button_1)
+                elif button_0 or button_1:
+                    control_cmd = control(0, 0, gain, 0, 0, 0, 0, button_0, button_1)
+                else:
+                    control_cmd = control(0, 0, gain, 0, 0, 0, 0, 0, 0)
+                rospy.loginfo(control_cmd)
+                pub.publish(control_cmd)
+                if not is_master_online():
+                    rospy.signal_shutdown("ROS master is offline")
+                rate.sleep()
 
 
 if __name__ == '__main__':
