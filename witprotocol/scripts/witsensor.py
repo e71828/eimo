b3ec10c13ae607cfff6308350b28305862112e6e
e71828 <740943170@qq.com>
2023-06-28 12:11:41 +0800

    robustness

--- witprotocol/scripts/witsensor.py
+++ witprotocol/scripts/witsensor.py
@@ -4,6 +4,7 @@ import serial
 import rospy
 from eimo_msgs.msg import angle
 from rosgraph import is_master_online
+from time import sleep
 
 if __name__ == '__main__':
     pub = rospy.Publisher('angle', angle, queue_size=1)
@@ -15,6 +16,7 @@ if __name__ == '__main__':
     baudrate = rospy.get_param('~baudrate', default=115200)
     with serial.Serial(serial_port, baudrate, timeout=1) as ser:
         while not rospy.is_shutdown():
+            yaw, roll, pitch, yaw_v, roll_v, pitch_v, yaw_a, roll_a, pitch_a = 0, 0, 0, 0, 0, 0, 0, 0, 0
             ser.reset_input_buffer()
             read_data = ser.read(44)
             packet_reader = PacketReader()
@@ -27,18 +29,17 @@ if __name__ == '__main__':
                         data_value.append(due_result)
                         if packet[0] == PacketID.POSITION:
                             roll, pitch, yaw = due_result
-                            rospy.loginfo('Roll  : {:5d}'.format(int(roll)))
-                            rospy.loginfo('Pitch : {:5d}'.format(int(pitch)))
-                            rospy.loginfo('Yaw   : {:5d}'.format(int(yaw)))
                         elif packet[0] == PacketID.VELOCITY:
                             roll_v, pitch_v, yaw_v = due_result
-                            # rospy.loginfo('angle velocity: {}'.format(due_result))
                         elif packet[0] == PacketID.ACCELERATION:
                             roll_a, pitch_a, yaw_a = due_result
-                            # rospy.loginfo('angle acceleration: {}'.format(due_result))
                         else:
                             pass
-                    pub.publish(yaw, roll, pitch, yaw_v, roll_v, pitch_v, yaw_a, roll_a, pitch_a)
-            if is_master_online():
-                rospy.signal_shutdown("ROS master is offline")
+            rospy.loginfo('Roll  : {:5d}'.format(int(roll)))
+            rospy.loginfo('Pitch : {:5d}'.format(int(pitch)))
+            rospy.loginfo('Yaw   : {:5d}'.format(int(yaw)))
+            # rospy.loginfo('angle acceleration: {}'.format(due_result))
+            # rospy.loginfo('angle velocity: {}'.format(due_result))
+
+            pub.publish(yaw=yaw, roll=roll, pitch=pitch)
             rate.sleep()
