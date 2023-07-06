c9b2da63a5ef37f8fc01fe644077189b6c46a7ad
e71828 <740943170@qq.com>
2023-07-06 22:57:49 +0800

    start with Ros2

--- eimo_msgs/scripts/eimo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /opt/ros/noetic/setup.bash
-source  /home/ubuntu/eimo_remote/devel/setup.sh
-
-export ROS_MASTER_URI=http://192.168.31.16:11311
-export ROS_IP=192.168.31.16
-exec "$@"
--- eimo_msgs/scripts/ros_local_auto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#python3 $HOME/eimo_remote/src/pca9685/src/pca9685/PCA9685.py
-screen -dmS scl bash -c 'source /home/ubuntu/.bashrc; roslaunch scl_passthrough serial_passthrough.launch'
-screen -dmS angle bash -c 'sleep 1; source /home/ubuntu/.bashrc; rosrun eimo_msgs witsensor.py'
-screen -dmS depth bash -c 'sleep 1; source /home/ubuntu/.bashrc; rosrun eimo_msgs depth_ms5837.py'
-screen -dmS pca bash -c 'sleep 1; source /home/ubuntu/.bashrc; rosrun pca9685 propelling.py'
-screen -dmS dive bash -c 'sleep 3; source /home/ubuntu/.bashrc; rosrun scl_passthrough diving.py'
